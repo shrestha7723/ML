27dd9e692a0d4520f9fef440af471af3a48e9ca3
Yosua Ian <yosua.ian.sebastian@gmail.com>
2017-09-22 13:39:50 +0700

    Added I/O for perceptron

--- python/Week_4_AND.py
+++ python/Week_4_AND.py
@@ -2,11 +2,33 @@
 import random as rand
 import numpy as np
 
-def sgn(input):
-    if input > 0:
+# importing sys for taking argument
+import sys
+
+
+def sgn(value):
+    if value > 0:
         return 1
     else:
         return 0
+
+
+def predict(inputs):
+    result = np.dot(inputs, weights)
+
+    result = sgn(result)
+
+    print("The predicted output is {}".format(result))
+
+
+def ask_input():
+    inputs = np.array([0,0,0])
+
+    for x in range(3):
+        inputs[x] = input("Enter input {} = ".format(x))
+
+    return inputs
+
 
 training_data = np.array([
 [0,0,0,0],
@@ -31,23 +53,28 @@ print("W1 = {}".format(w1))
 print("W2 = {}".format(w2))
 print("W3 = {}".format(w3))
 
-for j in range(10):
+for j in range(100):
     flag = 0
     for x in training_data:
         row = x[:3]
         target = x[3:][0]
 
-        output = np.dot(row,weights)
+        output = np.array([0,0,0])
+
+        for y in range(3):
+            output[y] = row[y] * weights[y]
+
+        output = np.sum(output)
+
+        #output = np.dot(row,weights)
 
         output = sgn(output)
 
-        #print("Aut = {}".format(output))
+        d_weights = np.array([0,0,0])
 
-        d_weight1 = learning_rate * (target - output) * row[0]
-        d_weight2 = learning_rate * (target - output) * row[1]
-        d_weight3 = learning_rate * (target - output) * row[2]
+        for k in range(len(row)):
+            d_weights[k] = learning_rate * (target - output) * row[k]
 
-        d_weights = np.array([d_weight1,d_weight2,d_weight3])
         erot = target - output
         print("T - O = {}".format(erot))
 
@@ -56,5 +83,16 @@ for j in range(10):
         if erot != 0:
             flag = 1
             #print("Looping...")
-    print("Done after {} loop".format(j+1))
-    break
+
+    if flag == 0:
+        print("Done after {} loop".format(j+1))
+        break
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'd':
+        predict(ask_input())
+
+    if sys.argv[1] == 'i':
+
+        inputs = [int(i) for i in sys.argv[2:5]]
+        predict(inputs)
