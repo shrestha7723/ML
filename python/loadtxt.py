c5ae150289c937a48edd008acd68b07d7a33010c
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2017-10-04 12:43:51 +0700

    Added new format for auto-mpg.data and how to load them.

--- python/loadtxt.py
+++ python/loadtxt.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
-data = np.loadtxt("../data/iris.data",dtype={'names': ('x1', 'x2', 'x3', 'x4','type')
-                                             ,'formats': ('f4', 'f4', 'f4', 'f4','S24')}
-                  ,delimiter=',')
+data = np.genfromtxt("../data/auto-mpg.csv"
+      ,dtype={'names': ('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8','type')
+             ,'formats': ('<f4', '<i4', '<f8', '<f4', '<f4', '<f4', '<i4', '<i2','|S32')}
+      ,missing_values='?'
+      ,delimiter=',')
 
 print(data[0])
