06a3347098c651b430e143f8370073f2ae1db14b
Yosua Ian <yosua.ian.sebastian@gmail.com>
2017-11-21 10:36:14 +0700

    Added breast-cancer dataset, plotted, and adding colorbar

---
+++ python/plot_kanker_dada.py
@@ -0,0 +1,114 @@
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rand
+from sklearn import svm, datasets
+
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+# import some data to play with
+# iris = datasets.load_iris()
+# Take the first two features. We could avoid this by using a two-dim dataset
+# X = iris.data[:, :2]
+# y = iris.target[:]
+
+features = [
+    'Sample code number',
+    'Clump Thickness',
+ 	'Uniformity of Cell Size',
+ 	'Uniformity of Cell Shape',
+	'Marginal Adhesion',
+ 	'Single Epithelial Cell Size',
+	'Bare Nuclei',
+ 	'Bland Chromatin',
+ 	'Normal Nucleoli',
+ 	'Mitoses',
+	'Class'
+    # Class 2 -> benign tumor
+    # Class 4 -> malignant tumor
+]
+
+for i in range(1):
+
+    rand.seed()
+    feature = rand.sample(range(1, 10), 2)
+    feature1 = feature[0]
+    feature2 = feature[1]
+
+    X = np.genfromtxt("../data/breast-cancer-wisconsin.data",delimiter=",",usecols=(feature1,feature2),missing_values='?',filling_values=0)
+    y = np.genfromtxt("../data/breast-cancer-wisconsin.data",delimiter=",",usecols=10)
+
+
+    # we create an instance of SVM and fit out data. We do not scale our
+    # data since we want to plot the support vectors
+    C = 1.0  # SVM regularization parameter
+    models = (svm.SVC(kernel='linear', C=C),
+              svm.LinearSVC(C=C),
+              svm.SVC(kernel='rbf', gamma=0.7, C=C),
+              svm.SVC(kernel='poly', degree=3, C=C))
+    models = (clf.fit(X, y) for clf in models)
+
+    # title for the plots
+    titles = ('SVC with linear kernel',
+              'LinearSVC (linear kernel)',
+              'SVC with RBF kernel',
+              'SVC with polynomial (degree 3) kernel')
+
+    # Set-up 2x2 grid for plotting.
+    fig, sub = plt.subplots(2, 2)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+    X0, X1 = X[:, 0], X[:, 1]
+    xx, yy = make_meshgrid(X0, X1)
+
+    for clf, title, ax in zip(models, titles, sub.flatten()):
+        im = plot_contours(ax, clf, xx, yy,
+                      cmap=plt.cm.coolwarm, alpha=0.8)
+        ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xlabel(features[feature1])
+        ax.set_ylabel(features[feature2])
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_title(title)
+
+    fig.colorbar(im)
+    plt.show()
